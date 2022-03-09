fa5ed024facd604781bcc99b7724a1b4723be54a
Edgar <Edgar@sias.com>
2022-03-09 19:46:47 +0800

    first commit

---
+++ src/main/java/Git.java
@@ -0,0 +1,11 @@
+/**
+ * @author Edgar
+ * @create 2022-03-09 19:43
+ * @faction:
+ */
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("你好git");
+    }
+}
