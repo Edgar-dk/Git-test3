f3a69b262e6c2a935f5f31847bef149dcd24621d
Edgar <Edgar@sias.com>
2022-03-09 20:21:17 +0800

    Thsied  commit

--- src/main/java/Git.java
+++ src/main/java/Git.java
@@ -8,5 +8,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("你好git");
         System.out.println("你好git2");
+        System.out.println("");
+        System.out.println("");
     }
 }
